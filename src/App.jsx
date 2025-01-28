ab2c1c562459e9dd2725411cf857bd47a9b2e164
kithogun666dunnel <pankajkumarrko05@gmail.com>
2025-01-28 16:28:05 +0530

    fetch cities data and update CityList component; enhance App routing to pass cities and loading state

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,53 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom"
-import Product from "./pages/product"
-import Pricing from "./pages/Pricing"
-import Homepage from "./pages/Homepage"
-import Login from "./pages/Login"
-import PageNotFound from "./pages/PageNotFound"
-import AppLayout from "./pages/AppLayout"
-import CityList from "./components/CityList"
-import CountryList from "./components/CountryList"
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Product from "./pages/product";
+import Pricing from "./pages/Pricing";
+import Homepage from "./pages/Homepage";
+import Login from "./pages/Login";
+import PageNotFound from "./pages/PageNotFound";
+import AppLayout from "./pages/AppLayout";
+import CityList from "./components/CityList";
+import CountryList from "./components/CountryList";
+import { useEffect, useState } from "react";
 
+const BASE_URL = "https://localhost:8000";
 
 function App() {
+  const [cities, setCities] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(function () {
+    async function fetchCities() {
+      try {
+        setIsLoading(true);
+        const res = await fetch(`${BASE_URL}/cities`);
+        const data = await res.json();
+        setCities(data);
+      } catch {
+        alert("ERROR LOADING THE DATA..");
+      } finally {
+        setIsLoading(false);
+      }
+    }
+    fetchCities();
+  }, []);
+
   return (
     <BrowserRouter>
-
       <Routes>
         <Route index element={<Homepage />} />
         <Route path="product" element={<Product />} />
         <Route path="pricing" element={<Pricing />} />
         <Route path="/login" element={<Login />} />
-        <Route path="app" element={<AppLayout />} >
-          <Route index element={<CityList />} />
-          <Route path="cities" element={<CityList />} />
+        <Route path="app" element={<AppLayout />}>
+          <Route index element={<CityList cities={cities} isLoading={isLoading} />} />
+          <Route path="cities" element={<CityList cities={cities} isLoading={isLoading} />} />
           <Route path="countries" element={<CountryList />} />
           <Route path="form" element={<p>form</p>} />
         </Route>
         <Route path="*" element={<PageNotFound />} />
       </Routes>
     </BrowserRouter>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/CityList.jsx
+++ src/components/CityList.jsx
@@ -1,6 +1,9 @@
 import styles from './CityList.module.css';
 
 function CityList() {
+
+
+
     return <ul className={styles.cityList}>LIST-CITIES</ul>;
 }
 
